022baea419972860b0d55e7e1a69bf1c6c76d125
nishadsatav13 <147748584+nishadsatav13@users.noreply.github.com>
2026-04-10 00:18:07 +0530

    Increase VOICE_THRESHOLD from 0.69 to 0.75

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ html, body, [class*="css"] {
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 USER_DIR = os.path.join(BASE_DIR, "data", "users")
 
-VOICE_THRESHOLD = 0.69
+VOICE_THRESHOLD = 0.75
 PHRASE_THRESHOLD = 0.50
 NUM_SAMPLES = 5
 
